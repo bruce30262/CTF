b592074aa3a4729785b96887e1b09f884a81faf4
Bruce Chen <bruce30262@pm.me>
2024-08-03 20:29:14 +0800

    Update arm tool setting script

--- script/set_arm_env.sh
+++ script/set_arm_env.sh
@@ -2,26 +2,36 @@
 
 set -ex
 
-# use qemu-user-static to emulate arm enviroment
+# use qemu-user to emulate arm enviroment
 # will install gcc & g++ toolchain, libc & libstdc++
 # download armtool.alias from https://github.com/bruce30262/CTF
+
+APT="apt"
+
+# If nala exist, use nala instead of apt
+if command -v nala &> /dev/null
+then
+    APT="nala"
+fi
+
+
+sudo $APT update
+
+# install g++ if not exist
+if ! command -v g++ &> /dev/null
+then
+    sudo $APT install -y g++
+fi
 
 cur_dir=$(dirname $(readlink -f $BASH_SOURCE))
 gxx_v=$(g++ -v  2>&1 | tail -1 | awk '{print $3}' | awk -F. '{print $1}')
 
-# qemu-arm-static
-sudo apt-get install -y qemu-user-static &&\
-# arm (gcc & libc)
-sudo apt-get install -y gcc-arm-linux-gnueabihf libc6-dev-armhf-cross gcc-arm-linux-gnueabi libc6-dev-armel-cross &&\
-# aarch64 (gcc & libc)
-sudo apt-get install -y gcc-aarch64-linux-gnu libc6-dev-arm64-cross &&\
-# arm (g++ & libstdc++)
-sudo apt-get install -y g++-arm-linux-gnueabihf libstdc++-$gxx_v-dev-armhf-cross g++-arm-linux-gnueabi libstdc++-$gxx_v-dev-armel-cross &&\
-# aarch64 (g++ & libstdc++)
-sudo apt-get install -y g++-aarch64-linux-gnu libstdc++-$gxx_v-dev-arm64-cross &&\
-
-# binfmt
-sudo apt-get install 'binfmt*'
+sudo $APT install -y qemu-user \
+    gcc-arm-linux-gnueabihf libc6-dev-armhf-cross gcc-arm-linux-gnueabi libc6-dev-armel-cross \
+    gcc-aarch64-linux-gnu libc6-dev-arm64-cross \
+    g++-arm-linux-gnueabihf libstdc++-$gxx_v-dev-armhf-cross g++-arm-linux-gnueabi libstdc++-$gxx_v-dev-armel-cross \
+    g++-aarch64-linux-gnu libstdc++-$gxx_v-dev-arm64-cross \
+    'binfmt*'
 
 set +x
 echo "source armtool.alias to apply the latest arm-toolchain setting."
